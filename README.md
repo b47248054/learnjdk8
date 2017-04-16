0b3150084c4620bd9864b9d3fb4e89729ca64c2e
wanye <wanye@meili-inc.com>
2017-04-16 22:13:47 +0800

    doc

--- README.md
+++ README.md
@@ -1,1 +1,147 @@
 # learnjdk8
+1.给接口增加非抽象方法
+```
+public interface Formula {
+    double calculate(int a);
+    default double sqrt(int a) {
+        return Math.sqrt(a);
+    }
+}
+
+public class FormulaImpl implements Formula {
+    @Override
+    public double calculate(int a) {
+        return sqrt(a * 100);
+    }
+
+    public static void main(String[] args) {
+        FormulaImpl formula = new FormulaImpl();
+        double calculate = formula.calculate(1);
+
+        double sqrt = formula.sqrt(16);
+        System.out.println(calculate + " " + sqrt);
+    }
+
+
+}
+```
+2. lambda 表达式
+```
+public class Sort {
+    static List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+
+    public static void sortFormual() {
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });
+    }
+
+    public static void sortLambda() {
+        Collections.sort(names, (a, b) -> b.compareTo(a));
+
+//        Collections.sort(names, Comparator.reverseOrder());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(names);
+        sortLambda();
+        System.out.println(names);
+        sortFormual();
+        System.out.println(names);
+
+        Converter<String, Integer> converter = (from -> Integer.valueOf(from));
+        Integer integer = converter.converter("123");
+        System.out.println(integer);
+
+        PersonFactory<Person> personFactory = Person::new;
+
+        Person person = personFactory.create("liu", "zhongxu");
+
+        System.out.println(person.firstName + person.lastName);
+
+    }
+
+
+}
+```
+3. 函数式接口访问
+```
+@FunctionalInterface
+public interface Converter<F,T> {
+    T converter(F from);
+}
+
+Converter<String, Integer> converter = (from -> Integer.valueOf(from));
+        Integer integer = converter.converter("123");
+        System.out.println(integer);
+```
+4. 方法与构造函数引用
+
+```
+public class Person {
+    String firstName;
+    String lastName;
+
+    Person() {
+
+    }
+
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+public interface PersonFactory<P extends  Person> {
+
+    P create(String firstName, String lastName);
+}
+
+        PersonFactory<Person> personFactory = Person::new;
+
+        Person person = personFactory.create("liu", "zhongxu");
+
+        System.out.println(person.firstName + person.lastName);
+//我们只需要使用 Person::new 来获取Person类构造函数的引用，Java编译器会自动根据PersonFactory.create方法的签名来选择合适的构造函数。
+```
+5. lambda作用域
+```
+public class LocalVariable {
+    public static void main(String[] args) {
+        int num = 1;//num必须不可被后面的代码修改（即隐性的具有final的语义）
+        Converter<Integer, String> converter = (from -> String.valueOf(from + num));
+        System.out.println(converter.converter(1));
+
+        LocalVariable localVariable = new LocalVariable();
+        localVariable.scopes();
+
+    }
+
+    public static int outerStaticNum;
+    int outerNum;
+
+    void scopes() {//lambda内部对于实例的字段以及静态变量是即可读又可写。
+        Converter<Integer,String> stringConverter = from -> {outerNum=32;return String.valueOf(from);};
+        Converter<Integer,String> stringConverter1 = from -> {outerStaticNum=99;return String.valueOf(from);};
+        System.out.println(stringConverter.converter(1)+outerNum);
+        System.out.println(stringConverter1.converter(1)+outerStaticNum);
+    }
+}
+```
+6. 访问接口的默认方法
+```
+see @InterfaceAccess
+//Predicate 接口只有一个参数，返回boolean类型。该接口包含多种默认方法来将Predicate组合成其他复杂的逻辑（比如：与，或，非）
+//Function 接口有一个参数并且返回一个结果，并附带了一些可以和其他函数组合的默认方法（compose, andThen）
+//Supplier 接口返回一个任意范型的值，和Function接口不同的是该接口没有任何参数
+//Consumer 接口表示执行在单个参数上的操作
+//Comparator 接口
+//Optional 接口 Optional 不是函数是接口，这是个用来防止NullPointerException异常的辅助类型
+//Stream 接口 java.util.Stream 表示能应用在一组元素上一次执行的操作序列。Stream 操作分为中间操作或者最终操作两种，最终操作返回一特定类型的计算结果，而中间操作返回Stream本身，这样你就可以将多个操作依次串起来。Stream 的创建需要指定一个数据源，比如 java.util.Collection的子类，List或者Set， Map不支持。Stream的操作可以串行执行或者并行执行。
+```
+7. Date Api
+8. Annotation
+9. CompletableFuture
--- src/main/java/com/liuzx/learnjdk8/InterfaceAccess.java
+++ src/main/java/com/liuzx/learnjdk8/InterfaceAccess.java
@@ -15,22 +15,22 @@ public class InterfaceAccess {
     //    predicate
     void predicateAccess() {
 
-        Predicate<String> predicate=s -> s.length()>0;
+        Predicate<String> predicate = s -> s.length() > 0;
         predicate.test("abc");
         predicate.negate().test("abc");
 
         Predicate<Boolean> nonNull = Objects::nonNull;
-        Predicate<Boolean> isNull=Objects::isNull;
+        Predicate<Boolean> isNull = Objects::isNull;
 
-        Predicate<String> isEmpty=String::isEmpty;
-        Predicate<String> isNotEmpty=isEmpty.negate();
+        Predicate<String> isEmpty = String::isEmpty;
+        Predicate<String> isNotEmpty = isEmpty.negate();
     }
 
     //    function
     void functionAccess() {
 
-        Function<String,Integer> toInteger = Integer::valueOf;
-        Function<String,String> backToString = toInteger.andThen(String::valueOf);
+        Function<String, Integer> toInteger = Integer::valueOf;
+        Function<String, String> backToString = toInteger.andThen(String::valueOf);
 
         backToString.apply("123");
     }
@@ -44,13 +44,13 @@ public class InterfaceAccess {
 
     //    consumer
     void consumerAccess() {
-        Consumer<Person> greeter = person -> System.out.println("I miss "+person.firstName+" "+person.lastName);
-        greeter.accept(new Person("bao","shali"));
+        Consumer<Person> greeter = person -> System.out.println("I miss " + person.firstName + " " + person.lastName);
+        greeter.accept(new Person("bao", "shali"));
     }
 
     //    comparator
     void comparatorAccess() {
-        Comparator<Person>  comparator = (p1,p2) -> p1.firstName.compareTo(p2.firstName);
+        Comparator<Person> comparator = (p1, p2) -> p1.firstName.compareTo(p2.firstName);
         Person p1 = new Person("liu", "zhongxu");
         Person p2 = new Person("bao", "shali");
         comparator.compare(p1, p2);
@@ -64,7 +64,7 @@ public class InterfaceAccess {
         optional.get();
         optional.orElse("bao");
 
-        optional.ifPresent(a-> System.out.println(a.charAt(0)));
+        optional.ifPresent(a -> System.out.println(a.charAt(0)));
     }
 
     //    stream
@@ -79,22 +79,23 @@ public class InterfaceAccess {
         stringCollection.add("bbb2");
         stringCollection.add("ddd1");
 
-//        filter
+//        filter 过滤通过一个predicate接口来过滤并只保留符合条件的元素，该操作属于中间操作，所以我们可以在过滤后的结果来应用其他Stream操作（比如forEach）。forEach需要一个函数来对过滤后的元素依次执行。forEach是一个最终操作，所以我们不能在forEach之后来执行其他Stream操作。
         stringCollection.stream().filter(s -> s.startsWith("b")).forEach(System.out::print);
         System.out.println("\nfilter end");
-//        sort
+//        sort 排序是一个中间操作，返回的是排序好后的Stream。如果你不指定一个自定义的Comparator则会使用默认排序。
         stringCollection.stream().sorted().forEach(System.out::print);
         System.out.println("\nsorted end");
+//        需要注意的是，排序只创建了一个排列好后的Stream，而不会影响原有的数据源，排序之后原数据stringCollection是不会被修改的：
         System.out.print(stringCollection);
         System.out.println("\nnon sorted end");
-//        map
+//        map 中间操作map会将元素根据指定的Function接口来依次将元素转成另外的对象，下面的示例展示了将字符串转换为大写字符串。你也可以通过map来讲对象转换成其他类型，map返回的Stream类型是根据你map传递进去的函数的返回值决定的。
         stringCollection.stream().map(String::toUpperCase).forEach(System.out::print);
         System.out.println("\nmap end");
-//        count
+//        count 计数是一个最终操作，返回Stream中元素的个数，返回值类型是long。
         long count = stringCollection.stream().filter(s -> s.startsWith("a")).count();
         System.out.print(count);
         System.out.println("\ncount end");
-//        reduce
+//        reduce 这是一个最终操作，允许通过指定的函数来讲stream中的多个元素规约为一个元素，规越后的结果是通过Optional接口表示的：
         Optional<String> reduce = stringCollection.stream().sorted().reduce((s1, s2) -> s1 + "#" + s2);
         reduce.ifPresent(System.out::print);
         System.out.println("\nreduce end");
@@ -109,14 +110,14 @@ public class InterfaceAccess {
             UUID uuid = UUID.randomUUID();
             values.add(uuid.toString());
         }
-//        串行
+//        串行 串行Stream上的操作是在一个线程中依次完成
         long t0 = System.nanoTime();
         long count = values.stream().count();
         System.out.println(count);
         long t1 = System.nanoTime();
         long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
         System.out.println(String.format("sequential sort took: %d ms", millis));
-//        并行
+//        并行 并行Stream则是在多个线程上同时执行。唯一需要做的改动就是将stream()改为parallelStream()。
         long t3 = System.nanoTime();
         count = values.parallelStream().count();
         System.out.println(count);
@@ -125,25 +126,25 @@ public class InterfaceAccess {
         System.out.println(String.format("parallel sort took: %d ms", millis));
     }
 
-    //    map
+    //    map Merge做的事情是如果键名不存在则插入，否则则对原键对应的值做合并操作并重新插入到map中。
     void mapAccess() {
         Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < 10; i++) {
-            map.putIfAbsent("val" + i,i);
+            map.putIfAbsent("val" + i, i);
         }
 
-        map.merge("val10", 100, (val,oval) -> ++val);
+        map.merge("val10", 100, (val, oval) -> ++val);
         System.out.println(map.get("val10"));
-        map.merge("val10", 100, (val,oval) -> ++val);
+        map.merge("val10", 100, (val, oval) -> ++val);
         System.out.println(map.get("val10"));
     }
+
     public static void main(String[] args) {
         InterfaceAccess interfaceAccess = new InterfaceAccess();
 //        interfaceAccess.consumerAccess();
 //
 //        interfaceAccess.optionalAccess();
 //        interfaceAccess.streamAccess();
-//
 //        interfaceAccess.mutiStreamAccess();
         interfaceAccess.mapAccess();
     }
